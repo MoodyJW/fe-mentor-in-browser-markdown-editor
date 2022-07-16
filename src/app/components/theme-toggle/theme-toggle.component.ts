0c8694aa95856167725e03c8fe7cbfe5fb90f33f
MoodyJW <jmoody1813@gmail.com>
2022-07-16 13:08:43 -0500

    styles done, functional but need to implement theme at app level first

--- src/app/components/theme-toggle/theme-toggle.component.ts
+++ src/app/components/theme-toggle/theme-toggle.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-theme-toggle',
   templateUrl: './theme-toggle.component.html',
-  styleUrls: ['./theme-toggle.component.scss']
+  styleUrls: ['./theme-toggle.component.scss'],
 })
 export class ThemeToggleComponent implements OnInit {
+  userTheme = window.matchMedia('(prefers-color-scheme: light)').matches
+    ? 'light-theme'
+    : 'dark-theme';
+  isLightTheme = this.userTheme === 'light-theme';
+  themeToggleControl = new FormControl(this.isLightTheme);
+  unsubscribe$ = new Subject();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.themeToggleControl.valueChanges
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((isLight: any) => {
+        this.isLightTheme = isLight;
+      });
   }
 
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+  }
 }
