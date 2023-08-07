632af0236c5145c7be9ee80f37a7ea08677f5001
MoodyJW <jmoody1813@gmail.com>
2023-08-07 15:23:40 -0700

    gh pages config for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAZkQGMBVrr0Fw6SkdiT5pbEAWunMVRN_g',
+    authDomain: 'browser-markdown-editor.firebaseapp.com',
+    projectId: 'browser-markdown-editor',
+    storageBucket: 'browser-markdown-editor.appspot.com',
+    messagingSenderId: '40671757549',
+    appId: '1:40671757549:web:e0f44efb8d1b0aa65db8e4',
+    measurementId: 'G-JCBX4MSFV8',
+  },
 };
